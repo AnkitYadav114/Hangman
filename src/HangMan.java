479344562c699f35df999f9c3835081b7efcb9ac
Whitroy <55481516+Whitroy@users.noreply.github.com>
2020-08-08 18:01:55 +0530

    Adding new riddles

--- src/HangMan.java
+++ src/HangMan.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class HangMan {
 
-    public void LoadLevel(int level,String challengeWord)
+    public void LoadLevel(int level,String challengeWord,String Description)
     {
         System.out.printf("\n\n%50s\n","Level "+level);
 
         HangMan.wait(1000);
 
-        System.out.println("Your Challenge :- \n");
+        System.out.println("Your Challenge :- "+Description+"\n");
 
         int totalChances=challengeWord.length();
 
@@ -39,17 +39,20 @@ public class HangMan {
             {
                 for (int i = 0; i <matchedIndex.length ; i++)
                 {
-                    guessWord.replace(matchedIndex[i],matchedIndex[i]+1, String.valueOf(letter.charAt(0)));
+                    if(matchedIndex[i]==0)
+                        guessWord.replace(matchedIndex[i],matchedIndex[i]+1, String.valueOf(letter.charAt(0)).toUpperCase());
+                    else
+                        guessWord.replace(matchedIndex[i],matchedIndex[i]+1, String.valueOf(letter.charAt(0)).toLowerCase());
                 }
             }
             System.out.println();
             Display(guessWord);
             if(CheckGameOverStatus(word,guessWord)){
-                System.out.println("\nCongratulation ! You won ");
+                System.out.println("\n\nCongratulation ! You won ");
                 break;
             }
             chances--;
-            System.out.println("\tChances Left :- "+chances+"\n");
+            System.out.println("\t\tChances Left :- "+chances+"\n");
         }
 
         if(chances==0 && !CheckGameOverStatus(word,guessWord))
@@ -114,12 +117,19 @@ public class HangMan {
 
         //Storing Data
         String[] data =new String[5];
-        data[0]="River";
-        data[1]="Sky";
-        data[2]="Water";
-        data[3]="Fish";
-        data[4]="Chicken";
+        data[0]="Emojis";
+        data[1]="Bank";
+        data[2]="Stairs";
+        data[3]="Rain";
+        data[4]="Stamp";
 
+        // Description
+        String[] desc =new String[5];
+        desc[0]="I have many faces, expressions and emotions and am only one tap away. What am I? ";
+        desc[1]="I have branches yet I have no leaves, no trunk and no fruit. What am I?";
+        desc[2]="I go up and I go down, sometimes I am curvy and sometimes I am straight. What am I? ";
+        desc[3]="What do you purposefully put lots of in and on your body, but run away from when you encounter it outside? ";
+        desc[4]="I go to and from India and Burma, Alaska and Nebraska and travel all over the world but I always stay in the corner. What am I?";
         System.out.print("Enter your Name ( Player 1 ):- ");
 
         //Declaring Scanner
@@ -135,7 +145,7 @@ public class HangMan {
         HangMan hg=new HangMan();
         for (int i = 0; i < 5; i++) {
             wait(1000);
-            hg.LoadLevel(i+1,data[i]);
+            hg.LoadLevel(i+1,data[i],desc[i]);
         }
 
         scannerObj.close();
